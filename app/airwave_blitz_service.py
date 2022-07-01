7fc4b86eb78a01a9af3e8151a467244c7acb8655
TheMadProphet <dkipi19@freeuni.edu.ge>
2022-07-01 13:31:13 +0400

    Remove unused files

--- app/airwave_blitz_service.py
+++
@@ -1,2 +0,0 @@
-class AirwaveBlitzService:
-    pass
--- tests/test_airwave_blitz_service.py
+++
@@ -1,9 +0,0 @@
-from app.airwave_blitz_service import AirwaveBlitzService
-
-
-def test_should_create_airwave_blitz_service() -> None:
-    assert AirwaveBlitzService() is not None
-
-
-def test_should_retrieve_interface_information() -> None:
-    pass
